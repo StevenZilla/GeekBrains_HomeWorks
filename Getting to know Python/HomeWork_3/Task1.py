5c21907835208d9e8fdb09142fc5806179da10e9
StePZay <125490388+StevenZilla@users.noreply.github.com>
2023-03-02 12:07:30 +0300

    Add HomeWork3
    Add HomeWork3

--- Getting to know Python/HomeWork_3/Task1.py
+++ Getting to know Python/HomeWork_3/Task1.py
@@ -1,5 +1,4 @@
-from random import randint
-n = int(input("Введите количество элементов массива: "))
-x = int(input("Введите искомое число X: "))
-list = [randint(0, n)]
-print([])
+n = int(input("Ввдеите число элементов: "))
+num_list = list(map(int, input("Введите элементы: ").split()))
+x = int(input("Введите искомое число: "))
+print(num_list.count(x))

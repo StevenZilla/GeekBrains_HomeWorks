5936bb1e659ebc7174beeb472fe49456a02adc93
StevenZilla <125490388+StevenZilla@users.noreply.github.com>
2023-03-07 19:40:42 +0300

    Add HomeWork4
    Add HomeWork4

--- Getting to know Python/HomeWork_3/Task2.py
+++ Getting to know Python/HomeWork_3/Task2.py
@@ -1,4 +1,5 @@
-n = int(input("Ввдеите число элементов: "))
-num_list = list(map(int, input("Введите элементы: ").split()))
-x = int(input("Введите искомое число: "))
-print(num_list.count(x))
+n = int(input()) 
+num_list = list(map(int, input().split())) 
+x = int(input()) 
+nums_new = [abs(x-i) for i in num_list] 
+print(num_list[nums_new.index(min(nums_new))])

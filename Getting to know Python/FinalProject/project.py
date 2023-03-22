b28e79fb14c15e05ef1d1dc48b228d894a53642c
StevenZilla <125490388+StevenZilla@users.noreply.github.com>
2023-03-23 00:20:42 +0300

    Add FinalProject
    Add FinalProject

---
+++ Getting to know Python/FinalProject/project.py
@@ -0,0 +1,9 @@
+# Задание 44 |
+# В ячейке ниже представлен код генерирующий DataFrame, которая состоит всего из 1 столбца. Ваша задача перевести его в one hot вид. Сможете ли вы это сделать без get_dummies?
+
+data['tmp'] = 1
+data.set_index([data.index, 'whoAmI'], inplace=True)
+data = data.unstack(level=-1, fill_value = 0).astype(int)
+data.columns = data.columns.droplevel()
+data.columns.name = None
+print(data)
